b5fd747a2726c388cc3d18dfa34b918a4fb88e2c
MoneSuper <mome9132@gmail.com>
2022-12-16 20:13:48 +0200

    PR from ItsMuri#6599

--- client/target.lua
+++ client/target.lua
@@ -30,5 +30,40 @@ if Config.Target == 'ox_target' then
         }
     })
 elseif Config.Target == 'qb-target' then
-    print('am lazy making into qb-target do a pull request if you want it')  -- unsupported
+    exports['qb-target']:AddBoxZone("pdupstair", vector3(459.46, -1008.02, 28.26), 1.4, 0.4, {
+        name="pdupstair",
+        heading=0,
+        --debugPoly=true,
+        minZ=27.26,
+        maxZ=29.66
+        }, {
+        options = { 
+            {
+                type = "client",
+                event = "serrulata-policevehicleshop:client:enter",
+                icon = 'fas fa-car',
+                label = 'Enter Police Shop',
+                job = 'police'
+            }
+        },
+        distance = 1.5,
+    })
+    exports['qb-target']:AddBoxZone("Pdout", vector3(212.67, -999.02, -99.0), 1.4, 0.2, {
+        name="pdunder",
+        heading=0,
+        --debugPoly=true,
+        minZ=-100.0,
+        maxZ=-97.6
+        }, {
+        options = { 
+            {
+                type = "client",
+                event = 'serrulata-policevehicleshop:client:leave',
+                icon = 'fas fa-car',
+                label = 'Leave Police Shop',
+                job = 'police'
+            }
+        },
+        distance = 1.5,
+    })
 end
